75df373bac43307d5ed5b388a0ac1728093bf66e
ewoudje <7384674+ewoudje@users.noreply.github.com>
2025-09-12 09:05:23 +0200

    Deployment scripts

--- deployment/install.sh
+++ deployment/install.sh
@@ -1,11 +1,11 @@
-#curl https://raw.githubusercontent.com/ewoudje/estore/refs/heads/main/install.sh | sh
+#!/bin/bash
+# UNTESTED
+
 
 # lxc-create -t download -n ewoud-estore -- -d alpine -r 3.22 -a amd64
 # lxc-attach -n ewoud-estore
+# inside lxc: curl https://raw.githubusercontent.com/ewoudje/estore/refs/heads/main/install.sh | sh
 
-# In lxc
-# rc-service crond start
-# rc-update add crond
 apk add curl
 wget https://raw.githubusercontent.com/ewoudje/estore/refs/heads/main/deployment/update.sh
 wget https://raw.githubusercontent.com/ewoudje/estore/refs/heads/main/deployment/estore.service
--- deployment/update.sh
+++ deployment/update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# UNTESTED
 
 touch current_version
 
