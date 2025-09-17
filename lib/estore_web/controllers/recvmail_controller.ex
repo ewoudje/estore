17b139483c6794e8e87299b5a78e189f39cb459a
ewoudje <7384674+ewoudje@users.noreply.github.com>
2025-09-17 16:15:12 +0200

    New v0.2.53

--- lib/estore_web/controllers/recvmail_controller.ex
+++ lib/estore_web/controllers/recvmail_controller.ex
@@ -1,8 +1,24 @@
 defmodule EstoreWeb.RecieveMailController do
   use EstoreWeb, :controller
 
-  def post(conn, input) do
-    IO.inspect(conn)
-    IO.inspect(input)
+  def post(conn, %{
+        "body-mime" => mime,
+        "subject" => subject,
+        "sender" => sender,
+        "recipient" => recipient,
+        "signature" => signature,
+        "timestamp" => timestamp,
+        "token" => token
+      }) do
+    mySignature =
+      :crypto.mac(:hmac, :sha256, Application.fetch_env!(:estore, :mail_key), timestamp <> token)
+      |> Base.encode16()
+
+    if mySignature != signature do
+      Conn.send_resp(401, "Unauthorized")
+    else
+      IO.inspect(subject)
+      Conn.send_resp(200, "Received")
+    end
   end
 end
