e61435b77335df03396113806fc11d6ead8938f7
ewoudje <7384674+ewoudje@users.noreply.github.com>
2025-09-24 05:56:55 +0200

    New v0.2.81

--- lib/estore_web/error_html.ex
+++ lib/estore_web/error_html.ex
@@ -1,8 +1,8 @@
 defmodule EstoreWeb.ErrorHTML do
   use EstoreWeb, :controller
 
-  def render(template, %{status: s, conn: conn, reason: err, stack: s}) do
-    Sentry.capture_exception(err, stacktrace: s)
-    Plug.Conn.send_resp(conn, s, "")
+  def render(_, %{status: status, stack: stack, conn: conn, reason: err}) do
+    Sentry.capture_exception(err, stacktrace: stack)
+    Plug.Conn.send_resp(conn, status, "")
   end
 end
